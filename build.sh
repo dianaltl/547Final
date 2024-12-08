063f4485c36a2af7bf1fc2bbf15c3b683404e753
dianaltl <terrazaslugod@arizona.edu>
2024-12-07 20:07:48 -0700

    Update build.sh due to health checks

--- build.sh
+++ build.sh
@@ -5,13 +5,10 @@ HUGO_VERSION="0.119.0"
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 tar -xzf hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 
-# Move Hugo to a user-accessible directory and update PATH
+# Move Hugo to a user-accessible directory
 mkdir -p ~/bin
 mv hugo ~/bin/hugo
 export PATH=$PATH:~/bin
 
-# Verify Hugo installation
-hugo version
-
-# Build the site with Hugo
-hugo -d src/public
+# Build the site
+hugo -d public
