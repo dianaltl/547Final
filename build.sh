f33ee27c9a25ba879a7df8d2bafbe8d5b61c7d91
dianaltl <terrazaslugod@arizona.edu>
2024-12-07 20:02:33 -0700

    Update build.sh to install Hugo in ~/bin

--- build.sh
+++ build.sh
@@ -4,7 +4,14 @@
 HUGO_VERSION="0.119.0"
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 tar -xzf hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-mv hugo /usr/local/bin/hugo
 
-# Run Hugo to build the site
+# Move Hugo to a user-accessible directory and update PATH
+mkdir -p ~/bin
+mv hugo ~/bin/hugo
+export PATH=$PATH:~/bin
+
+# Verify Hugo installation
+hugo version
+
+# Build the site with Hugo
 hugo -d src/public
